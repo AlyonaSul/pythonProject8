8a5d87c8c8ec8260de68603eaadee577246bf0c5
Alyona S <alyonasul6277@gmail.com>
2022-07-30 21:35:51 +0300

    Правильный подсчёт скидки

--- Work2.py
+++ Work2.py
@@ -1,19 +1,17 @@
-tickets = input('Введите количество билетов:')
-sum = 0
+tickets = int(input("Введите количество билетов: "))
+sum: int = 0
 
 for i in range(1, tickets+1):
-    person_age = input(int("Введите возраст посетителя"))
+    person_age = int(input("Введите возраст посетителя: "))
     if person_age < 18:
         print ("Вход бесплатный")
-    else:
-        if 18 <= person_age <= 25:
+    elif 18 <= person_age <= 25:
             sum += 990
-        elif person_age > 25:
+    elif person_age > 25:
             sum += 1390
 
-print ("Сумма к оплате:" + str(sum))
-
 if tickets > 3:
-    sum = sum/100*10
-
-print("Сумма к оплате со скидкой:")
+    sum = int(sum- (sum/100*10))
+    print("Сумма к оплате со скидкой: " + str(sum) + "руб")
+else:
+    print ("Сумма к оплате:" + str(sum))
